707038b3cbd2e7f9afc8cc632e9cb53d4bd71aa5
Zahar Bystrok <zbystrok.jr@gmail.com>
2020-09-29 08:32:33 +0300

    init: start typical arrays problems-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minValue = 0
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (minValue > array[i]) 
+        minValue = array[i]
+    }
+    return minValue
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxValue = 0
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (maxValue < array[i]) 
+        maxValue = array[i]
+    }
+    return maxValue
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0
+  let average = 0
+  if (array === undefined || array.length == 0) {
+    return 0
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    average = sum / array.length
+    return average
+  }
 }
